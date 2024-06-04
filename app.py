f99b063318ec6b40c5298e5da6e34a8d0f30fbdf
stingtam <stingtam@tom.com>
2024-06-04 16:14:22 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask
-from markupsafe import escape
+from faker import Faker
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+name = Faker().name()
+movies = [
+    {"title": "My Neighbor Totoro", "year": "1988"},
+    {"title": "Dead Poets Society", "year": "1989"},
+    {"title": "A Perfect World", "year": "1993"},
+    {"title": "Leon", "year": "1994"},
+    {"title": "Mahjong", "year": "1996"},
+    {"title": "Swallowtail Butterfly", "year": "1996"},
+    {"title": "King of Comedy", "year": "1999"},
+    {"title": "Devils on the Doorstep", "year": "1999"},
+    {"title": "WALL-E", "year": "2008"},
+    {"title": "The Pork of Music", "year": "2012"},
+]
+
 
 @app.route("/")
-def hello():
-    return "Welcome to My Watchlist!"
-
-
-@app.route("/user/<name>")
-def user_page(name):
-    return f"User: {escape(name)}"
+def index():
+    return render_template("index.html", name=name, movies=movies)
