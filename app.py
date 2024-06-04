8f511d28d956ce301d297330f809d36b4d1f8e23
stingtam <stingtam@tom.com>
2024-06-04 17:43:34 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,21 +1,63 @@
+import os
+import sys
+
+import click
 from faker import Faker
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith("win")
+if WIN:
+    prefix = "sqlite:///"
+else:
+    prefix = "sqlite:////"
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = prefix + os.path.join(app.root_path, "data.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-name = Faker().name()
-movies = [
-    {"title": "My Neighbor Totoro", "year": "1988"},
-    {"title": "Dead Poets Society", "year": "1989"},
-    {"title": "A Perfect World", "year": "1993"},
-    {"title": "Leon", "year": "1994"},
-    {"title": "Mahjong", "year": "1996"},
-    {"title": "Swallowtail Butterfly", "year": "1996"},
-    {"title": "King of Comedy", "year": "1999"},
-    {"title": "Devils on the Doorstep", "year": "1999"},
-    {"title": "WALL-E", "year": "2008"},
-    {"title": "The Pork of Music", "year": "2012"},
-]
+# 初始化扩展，传入程序实例 app
+db = SQLAlchemy(app)
+
+
+# 保存两类数据：1用户信息；2电影条目信息。
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+    name = Faker().name()
+    movies = [
+        {"title": "My Neighbor Totoro", "year": "1988"},
+        {"title": "Dead Poets Society", "year": "1989"},
+        {"title": "A Perfect World", "year": "1993"},
+        {"title": "Leon", "year": "1994"},
+        {"title": "Mahjong", "year": "1996"},
+        {"title": "Swallowtail Butterfly", "year": "1996"},
+        {"title": "King of Comedy", "year": "1999"},
+        {"title": "Devils on the Doorstep", "year": "1999"},
+        {"title": "WALL-E", "year": "2008"},
+        {"title": "The Pork of Music", "year": "2012"},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m["title"], year=m["year"])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo("Done.")
 
 
 @app.route("/")
